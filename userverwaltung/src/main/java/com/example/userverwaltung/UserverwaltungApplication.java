84cf9dea2003a164c79294b1696c5f8edbb60d53
Paul <p.schoeppl@htlstp.at>
2023-03-09 08:29:06 +0100

    Userverwaltung hinzugefügt (fehlende Files)

---
+++ userverwaltung/src/main/java/com/example/userverwaltung/UserverwaltungApplication.java
@@ -0,0 +1,13 @@
+package com.example.userverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserverwaltungApplication.class, args);
+    }
+
+}
---
+++ userverwaltung/src/test/java/com/example/userverwaltung/UserverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserverwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
